b45672007c4fadfd825f74419df4180d36e405bd
Yelyzaveta Shcherbachenko <sherbachenkoliza@gmail.com>
2021-09-24 16:52:35 +0300

    Done cutStrings task

--- src/module-1/cutStrings/index.js
+++ src/module-1/cutStrings/index.js
@@ -1,1 +1,11 @@
-export const cutStrings = (arr = []) => {};
+export const cutStrings = (arr = []) => {
+    let arrLengths = [];
+    
+    for (let i = 0; i < arr.length; i++){
+        arrLengths.push(arr[i].length);
+    }
+    
+    let minLength = Math.min(...arrLengths);
+    
+    return arr.map(el => el.slice(0, minLength));
+};
