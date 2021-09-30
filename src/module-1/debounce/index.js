9890bfa7f92039658bf95a31e0973c73d1829877
Yelyzaveta Shcherbachenko <sherbachenkoliza@gmail.com>
2021-09-30 12:45:37 +0300

    update files

--- src/module-1/debounce/index.js
+++ src/module-1/debounce/index.js
@@ -9,26 +9,3 @@ export const debounce = (fn, delay = 0) => {
         setTimeout(()=> isFrozen = false, delay);
     }
 }
-
-const obj = {
-    nickname: 'John',
-    getName () {
-      return this.nickname;
-    }
-  }
-
-  const debounceLog = debounce((a, b) => {
-    return a + b;
-  }, 1000);
-
-  const debounceGetName = debounce(obj.getName);
-
-  // Note: should log "1" only one time
-  console.error(debounceLog(1, 2));
-  console.error(debounceLog(1, 3));
-  console.error(debounceLog(1, 4));
-
-  // Note: should log "John" only one time
-  console.error(debounceGetName.call(obj));
-  console.error(debounceGetName.call(obj));
-  console.error(debounceGetName.call(obj));
